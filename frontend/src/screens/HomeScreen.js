aa602183abc9140b4dfe76057586d4717e2f5790
buivanhieu-it <hieubuivan1>
2022-03-04 14:03:29 +0700

    use-data-in-fe-to-continue-and-add-bootstrap

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,7 @@ import { useEffect, useReducer, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import logger from 'use-reducer-logger';
+import data from '../data';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -45,7 +46,7 @@ function HomeScreen() {
         ) : error ? (
           <div>{error}</div>
         ) : (
-          products.map((product) => (
+          data.products.map((product) => (
             <div className="product" key={product.slug}>
               <Link to={`/product/${product.slug}`}>
                 <img src={product.image} alt={product.name}></img>
